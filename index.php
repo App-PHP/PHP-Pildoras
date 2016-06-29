9b804a90ede0acb8b6424141d4e924cfee67cc7a
Hernan Velasquez <hernanudea@gmail.com>
2016-06-29 18:05:03 -0500

    Video 15, Operador ternario

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 require("config/config.php");
 require("html/head.html");
-require("videos/1/15-CondicionalesYPrioridadOperadores.php");
+require("videos/1/16-OperadorTernario.php");
 include("html/foot.html");
