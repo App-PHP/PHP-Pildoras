977ba356856f29c0a229422f3bcbf09cab285edb
Hernan Velasquez <hernanudea@gmail.com>
2016-07-20 20:20:34 -0500

    Video 26. Modificadores de acceso

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 require("config/config.php");
 require("html/head.html");
-require("videos/2/25-Herencia.php");
+require("videos/2/26-ModificadoresAcceso.php");
 include("html/foot.html");
---
+++ videos/2/26-ModificadoresAcceso.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hernan
+ * Date: 20/07/16
+ * Time: 19:25 PM
+ */
+
+echo "<h2>Modificadores de acceso</h2>";
+
+echo "- Modularidad, difivir en modulos, dividir en clases.<br>";
+
+echo "- Encapsulación, 'esconder' los metodos y atributos para otras clases.<br>";
+
+echo "- Modificadores de acceso, definen quienes pueden acceder a cada metodo y cada atributo.<br>";
+
+echo "- <code>public</code> Accesible desde cualquier parte.<br>";
+echo "- <code>private</code> Accesible solo desde la misma clase.<br>";
+echo "- <code>protected</code> Accesible sólo desde la misma clase y las clases herederas o hijas.<br>";
+
+echo "- esto genera un error, ya que se está intentando asignar un valor a un atributo private.<br>";
+
+
+echo "- Ejemplo.<br><br><br>";
+
+require("PersonaConModificarAcceso.php");
+
+$alumno1 = new Alumno();
+
+$alumno1->semestre=55; //
+//echo $alumno1->semestre;
